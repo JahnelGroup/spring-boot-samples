779c0430658504523b5b70484822260360fed195
Steven Zgaljic <szgaljic@jahnelgroup.com>
2017-09-28 15:56:56 -0500

    moved from multi to single

---
+++ spring-boot-elasticsearch/backend/injestor/src/main/kotlin/com/jahnelgroup/elasticsearch/injestor/person/Person.kt
@@ -0,0 +1,7 @@
+package com.jahnelgroup.elasticsearch.injestor.person
+
+data class Person(
+        var id : Long = 1,
+        var firstName : String,
+        var lastName : String
+)
---
+++ spring-boot-elasticsearch/backend/injestor/src/main/kotlin/com/jahnelgroup/elasticsearch/injestor/person/PersonRepo.kt
@@ -0,0 +1,2 @@
+package com.jahnelgroup.elasticsearch.injestor.person
+
