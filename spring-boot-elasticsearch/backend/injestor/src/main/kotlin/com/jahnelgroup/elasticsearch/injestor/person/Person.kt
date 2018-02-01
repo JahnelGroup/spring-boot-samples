bf4e168b4eabdd3bb2f3f6623374cff4d0c07efd
szgaljic <Steven Zgaljic>
2018-01-31 22:53:16 -0600

    removed legacy elasticsearch sample

--- spring-boot-elasticsearch/backend/injestor/src/main/kotlin/com/jahnelgroup/elasticsearch/injestor/person/Person.kt
+++
@@ -1,7 +0,0 @@
-package com.jahnelgroup.elasticsearch.injestor.person
-
-data class Person(
-        var id : Long = 1,
-        var firstName : String,
-        var lastName : String
-)
--- spring-boot-elasticsearch/backend/injestor/src/main/kotlin/com/jahnelgroup/elasticsearch/injestor/person/PersonRepo.kt
+++
@@ -1,2 +0,0 @@
-package com.jahnelgroup.elasticsearch.injestor.person
-
