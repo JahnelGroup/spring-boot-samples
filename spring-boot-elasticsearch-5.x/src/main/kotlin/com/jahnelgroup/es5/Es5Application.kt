9f275d32f39972ea0c5e3d85b91062e0906dbed0
szgaljic <Steven Zgaljic>
2018-02-10 23:09:27 -0600

    Removed command line runner

--- spring-boot-elasticsearch-5.x/src/main/kotlin/com/jahnelgroup/es5/Es5Application.kt
+++ spring-boot-elasticsearch-5.x/src/main/kotlin/com/jahnelgroup/es5/Es5Application.kt
@@ -1,16 +1,10 @@
 package com.jahnelgroup.es5
 
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Es5Application : CommandLineRunner {
-    override fun run(vararg args: String?) {
-        println("=========== Running ============")
-    }
-
-}
+class Es5Application 
 
 fun main(args: Array<String>) {
     runApplication<Es5Application>(*args)
