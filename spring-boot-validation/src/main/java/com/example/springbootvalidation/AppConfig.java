b6b44ab81ced4051f29a6fe7ecd12c00d05ab180
Steven Zgaljic <szgaljic@jahnelgroup.com>
2018-03-27 09:46:12 -0500

    added codes

---
+++ spring-boot-validation/src/main/java/com/example/springbootvalidation/AppConfig.java
@@ -0,0 +1,21 @@
+package com.example.springbootvalidation;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class AppConfig {
+
+    /**
+     * Makes the codes in messages.properties available to Hibernate validator.
+     */
+    @Bean
+    public LocalValidatorFactoryBean validator(MessageSource messageSource) {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource);
+        return bean;
+    }
+
+}
