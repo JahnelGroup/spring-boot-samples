eb05b90706c9fa60d38df928855eea8d0349be06
szgaljic <Steven Zgaljic>
2018-06-22 20:47:16 -0500

    updated

--- spring-boot-integration-http/src/main/kotlin/com/example/demo/payment/PaymentRepo.kt
+++ spring-boot-integration-http/src/main/kotlin/com/example/demo/payment/PaymentRepo.kt
@@ -1,6 +1,5 @@
 package com.example.demo.payment
 
-import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 
 interface PaymentRepo : CrudRepository<Payment, Long>
