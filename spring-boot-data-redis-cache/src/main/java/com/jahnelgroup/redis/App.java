da01636c318275fd81ece993a94b97bd67bcb91f
Steven Zgaljic <szgaljic@jahnelgroup.com>
2019-01-04 14:51:38 -0600

    removed unused annotation

--- spring-boot-data-redis-cache/src/main/java/com/jahnelgroup/redis/App.java
+++ spring-boot-data-redis-cache/src/main/java/com/jahnelgroup/redis/App.java
@@ -1,11 +1,9 @@
 package com.jahnelgroup.redis;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-@Slf4j
 @EnableCaching
 @SpringBootApplication
 public class App {
