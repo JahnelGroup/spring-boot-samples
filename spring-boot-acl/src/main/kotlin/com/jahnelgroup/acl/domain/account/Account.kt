a4bd3258ce739c039b444a5f9147d4fe17bcb725
Steven Zgaljic <szgaljic@gmail.com>
2019-01-18 20:14:03 -0600

    updated acl

--- spring-boot-acl/src/main/kotlin/com/jahnelgroup/acl/domain/account/Account.kt
+++ spring-boot-acl/src/main/kotlin/com/jahnelgroup/acl/domain/account/Account.kt
@@ -1,15 +1,16 @@
 package com.jahnelgroup.acl.domain.account
 
 import com.jahnelgroup.acl.domain.AbstractEntity
+import com.jahnelgroup.acl.domain.bank.Bank
 import com.jahnelgroup.acl.domain.user.User
 import com.jahnelgroup.springframework.security.acl.annotations.AclAce
+import com.jahnelgroup.springframework.security.acl.annotations.AclParent
 import com.jahnelgroup.springframework.security.acl.annotations.AclSecured
 import com.jahnelgroup.springframework.security.acl.annotations.AclSid
 import javax.persistence.*
 import javax.validation.constraints.NotNull
 
 @Entity
-@AclSecured
 @Table(name = "accounts")
 data class Account(
 
